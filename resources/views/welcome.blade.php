bae9ca6d6c839ae0b235a1de2a85266bd727101f
NesTor Almendras <nestor.almendras.lopez@hotmail.com>
2019-03-26 16:54:16 -0400

    se modifico el welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Version  Prueba
                 </div>
             </div>
         </div>
